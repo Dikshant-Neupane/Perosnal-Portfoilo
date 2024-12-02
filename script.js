50489b51fb52e971831f779e53a0c768eae763d9
Dikshant-Neupane <dikshantneupane690@gmail.com>
2024-12-02 21:33:36 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const toggleButton = document.getElementById('mode-toggle');
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    if (body.classList.contains('dark-mode')) {
+        toggleButton.textContent = '🌞 Light Mode';
+    } else {
+        toggleButton.textContent = '🌜 Dark Mode';
+    }
+});
